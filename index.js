4663f12482025cf4bff97554364fbdc5abc0dcba
er10n2 <erionnttelaku@hotmail.com>
2025-12-31 15:20:12 +0100

    added some csss style

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ let refreshEl = document.getElementById("refresh");
 let img1 = document.getElementById("player1-img");
 let img2 = document.getElementById("player2-img");
 
+let rollBtn = document.getElementById("roll-btn");
+
 function Roll(){
 
   let  random1 = Math.floor(Math.random()*6)+1;
   let  random2 = Math.floor(Math.random()*6)+1;
 
-  img1.setAttribute("src", `images/dice${random1}.png`) ;
-  img2.setAttribute("src", `images/dice${random2}.png`) ;
+  img1.setAttribute("src", `dice${random1}.png`) ;
+  img2.setAttribute("src", `dice${random2}.png`) ;
 
 
   if( random1 > random2){
@@ -25,4 +27,4 @@ function Roll(){
 
 
 }
-window.onload = Roll;
+rollBtn.addEventListener("click", Roll);
